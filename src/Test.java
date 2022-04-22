426f25c3bc2041820e91f08d1bb394cd435708e0
pauloud <57322619+pauloud@users.noreply.github.com>
2022-04-22 17:07:44 +0200

    Delete Test.java

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-package src;
-public class Test {
-    public static void main (String [] args){
-        Sandwich<Baguette,Mayonnaise,Salade> sandwich = new Sandwich<>(); 
-        Sandwich<Baguette,Mayonnaise,ListeIngredients> sandwich2 = new Sandwich<>(); 
-
-    }
-    
-}
